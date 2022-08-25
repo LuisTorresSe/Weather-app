0baa751c39e934bb79cf0f1096e6cd681f63a4f4
luistorresse <luistorresse97@gmail.com>
2022-08-24 21:21:54 -0500

    actualizacion

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
-const API=process.env.REACT_APP_API
-const TOKEN=process.env.REACT_APP_TOKEN
+const API = process.env.REACT_APP_API;
+const TOKEN = process.env.REACT_APP_TOKEN;
 
-export function Api(city){
-    return (fetch(`${API}&key=${TOKEN}&q=${city}&days=3&aqi=no&alerts=no`).then(res => res.json()));
-    
+export function Api(city) {
+  return fetch(`${API}&key=${TOKEN}&q=${city}&days=3&aqi=no&alerts=no`).then(
+    (res) => res.json()
+  );
 }
